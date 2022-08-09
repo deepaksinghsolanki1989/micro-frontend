8c3840611403c06ec23595924155a926c14aaa39
Deepak Singh Solanki <deepaksinghsolanki1989@gmail.com>
2022-08-09 23:29:10 +0530

    Initial Commit

---
+++ App1/webpack.config.js
@@ -0,0 +1,46 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+module.exports = {
+  entry: path.resolve(__dirname, "src", "index.tsx"),
+  target: "web",
+  mode: "development",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".json", ".ts", ".tsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        loader: "ts-loader",
+        options: {
+          compilerOptions: {
+            noEmit: false,
+          },
+        },
+        exclude: /node_modules/,
+      },
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        loader: "source-map-loader",
+      },
+      {
+        test: /\.css$/,
+        loader: "css-loader",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+    }),
+    new MiniCssExtractPlugin({
+      filename: "./src/yourfile.css",
+    }),
+  ],
+};
---
+++ App2/src/App.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { FC } from "react";
+
+const App: FC = () => {
+  return <h1>App 1</h1>;
+};
+
+export default App;
